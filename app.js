1456bcee2d4164f89712966166d0c3590d0bb348
Sharon Kim <carolinek@SKs-MacBook-Air.local>
2020-08-06 23:42:40 -0400

    added media query and function to hide video

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ $(() => {
     // ==== VARIABLES ==== //
     const $learnBtn = $('#learn-btn')
     const $video = $('.video')
+    const $closeBtn = $('#close-btn')
+
 
     // ==== FUNCTIONS & EVENT HANDLERS ==== //
     const playVideo = () => {
         // console.log("it works!")
-        $video.css('display', 'block')
+        $video.show();
+    }
+    
+    const hideVideo = () => {
+        $video.hide();
     }
 
     // ==== EVENT LISTENERS ==== //
     $learnBtn.on('click', playVideo);
+    $closeBtn.on('click', hideVideo);
 })
